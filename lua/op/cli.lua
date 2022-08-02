3b036740bcb0c466bfc761e570b6879881d21b82
Mat Jones <mat@mjones.network>
2022-08-02 08:41:41 -0400

    use vim.fn.system instead of vim.fn.jobstart

--- lua/op/cli.lua
+++ lua/op/cli.lua
@@ -49,48 +49,15 @@ local function build_cmd(full_cmd)
     local full_cmd_args = vim.list_extend(vim.deepcopy(full_cmd), vim.list_extend(vim.deepcopy(global_args), args))
     table.insert(full_cmd_args, 1, 'op')
 
-    -- TODO remove this workaround
-    -- TODO for some reason `op item create` hangs when run through job control but not `vim.fn.system()`
-    if full_cmd_args[2] == 'item' and full_cmd_args[3] == 'create' then
-      local escaped_args = vim.tbl_map(function(arg)
-        return vim.fn.shellescape(arg)
-      end, full_cmd_args)
-      local output = vim.fn.systemlist(table.concat(escaped_args, ' '))
-      local exit_code = vim.deepcopy(vim.v.shell_error or 0)
-      if exit_code ~= 0 then
-        -- non-zero exit code, return output in stderr position
-        return {}, output, exit_code
-      else
-        -- zero exit code, return output in stdout position
-        return output, {}, exit_code
-      end
+    local output = non_empty_values(vim.fn.systemlist(full_cmd_args))
+    local exit_code = vim.deepcopy(vim.v.shell_error or 0)
+    if exit_code ~= 0 then
+      -- non-zero exit code, return output in stderr position
+      return {}, output, exit_code
+    else
+      -- zero exit code, return output in stdout position
+      return output, {}, exit_code
     end
-
-    local stdout = {}
-    local stderr = {}
-    local exit_code = nil
-    local job_id = vim.fn.jobstart(full_cmd_args, {
-      stdout_buffered = true,
-      stderr_buffered = true,
-      detach = false,
-      on_stdout = function(_, data)
-        stdout = vim.list_extend(stdout, non_empty_values(data))
-      end,
-      on_stderr = function(_, data)
-        stderr = vim.list_extend(stderr, non_empty_values(data))
-      end,
-      on_exit = function(job_exit_code)
-        exit_code = job_exit_code
-      end,
-    })
-    local status = vim.fn.jobwait({ job_id }, JOB_TIMEOUT)[1]
-    -- see :h jobwait
-    if status == -1 then
-      vim.notify_once(
-        'Command with args ' .. vim.inspect(full_cmd_args) .. ' timed out after ' .. (JOB_TIMEOUT / 1000) .. ' seconds'
-      )
-    end
-    return stdout, stderr, exit_code
   end
 end
 
