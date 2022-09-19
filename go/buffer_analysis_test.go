ff218204a245f1527869b678cf63149a3d1f12ed
Mat Jones <mat@mjones.network>
2022-09-19 12:54:10 -0400

    add tests

--- go/buffer_analysis_test.go
+++ go/buffer_analysis_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -8,17 +10,81 @@ func (a AsyncManager) MockExecutor(exe Executor) {
 	a.execLua = exe
 }
 
-func FormatSecretType_WithItemTitle(t *testing.T) {
-	Async.MockExecutor(func(lua string) {})
-	// TODO
+func TestFormatSecretType_WithItemTitle(t *testing.T) {
+	pattern := FieldPattern{
+		ItemTitle:  "Item Title",
+		FieldTitle: "Field Title",
+	}
+	formatted := formatSecretType(pattern)
+	expected := "Item Title Field Title"
+	if formatted != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, formatted)
+	}
 }
 
-func FormatSecretType_WithoutItmeTitle(t *testing.T) {
-	Async.MockExecutor(func(lua string) {})
-	// TODO
+func TestFormatSecretType_WithoutItemTitle(t *testing.T) {
+	pattern := FieldPattern{
+		FieldTitle: "Field Title",
+	}
+	formatted := formatSecretType(pattern)
+	expected := "Field Title"
+	if formatted != expected {
+		t.Errorf("Expected '%s', '%s'", expected, formatted)
+	}
 }
 
-func AnalyzeBufferTest(t *testing.T) {
-	Async.MockExecutor(func(lua string) {})
-	// TODO
+func lineDiagnosticRequestsFromStr(text string) []LineDiagnosticRequest {
+	lines := strings.Split(text, "\n")
+	requests := make([]LineDiagnosticRequest, len(lines))
+	for linenr, line := range lines {
+		requests = append(requests, LineDiagnosticRequest{
+			LineNr: linenr,
+			Text:   line,
+		})
+	}
+	return requests
+}
+
+func TestAnalyzeBuffer(t *testing.T) {
+	requests := lineDiagnosticRequestsFromStr(`
+Some text
+
+Some more text
+
+A fake GitHub token is ghr_aU73Wj7Jow3qAuQfuOaU73Wj7Jow3qAuQfuOaU73Wj7Jow3qAuQfuOaU73Wj7Jow3qAuQfuOa7wQ
+
+A fake credit card number is 4222222222222
+
+Emails are ignored because they create too many false positives: test@example.com
+
+Regular URLs are also ignored because they aren't necessarily sensitive: https://github.com
+
+A fake Slack webhook URL is https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+	`)
+
+	results := analyzeBuffer(requests)
+	expected := []LineDiagnostic{
+		{
+			Line:       5,
+			ColStart:   23,
+			ColEnd:     103,
+			SecretType: "GitHub token",
+		},
+		{
+			Line:       7,
+			ColStart:   29,
+			ColEnd:     42,
+			SecretType: "credit card",
+		},
+		{
+			Line:       13,
+			ColStart:   28,
+			ColEnd:     105,
+			SecretType: "Slack webhook",
+		},
+	}
+
+	if !reflect.DeepEqual(expected, results) {
+		t.Errorf("Expected:\n  %+v \nGot:\n  %+v", expected, results)
+	}
 }
